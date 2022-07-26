72a3bd6d9579a78bd982d99237de091e765dbee9
jevazquezb <je.vazquezb@gmail.com>
2022-07-26 13:12:19 -0500

    Update header

--- src/components/header.js
+++ src/components/header.js
@@ -3,10 +3,11 @@ import { Icon } from '@iconify/react';
 function Header() {
   return (
     <header>
+      <Icon icon="eva:arrow-ios-back-fill" color="white" width="18" height="18" />
       <h1>Carbon Intensity in the UK</h1>
       <nav>
-        <Icon icon="mdi:microphone" color="white" width="50" height="50" />
-        <Icon icon="mdi:cog" color="white" width="50" height="50" />
+        <Icon icon="mdi:microphone" color="white" className="size" />
+        <Icon icon="mdi:cog" color="white" className="size" />
       </nav>
     </header>
   );
