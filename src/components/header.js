bfe6302966258354eeb27bdf926cae0396904732
jevazquezb <je.vazquezb@gmail.com>
2022-07-26 14:33:06 -0500

    Add return arrow

--- src/components/header.js
+++ src/components/header.js
@@ -6,8 +6,8 @@ function Header() {
       <Icon icon="eva:arrow-ios-back-fill" color="white" width="18" height="18" />
       <h1>Carbon Intensity in the UK</h1>
       <nav>
-        <Icon icon="mdi:microphone" color="white" className="size" />
-        <Icon icon="mdi:cog" color="white" className="size" />
+        <Icon icon="mdi:microphone" color="white" className="nav-size" />
+        <Icon icon="mdi:cog" color="white" className="nav-size" />
       </nav>
     </header>
   );
