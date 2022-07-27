13a912c832a21b5d134fc17fb13be800b87df41c
jevazquezb <je.vazquezb@gmail.com>
2022-07-27 03:00:57 -0500

    Add regions reducer

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import countryReducer from './country/country';
+import regionReducer from './region/region';
 
 const store = configureStore({
   reducer: {
     country: countryReducer,
+    region: regionReducer,
   },
 });
 
