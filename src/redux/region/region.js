f218dc333d11c82d4d2ec49856a127e0bf9c8355
jevazquezb <je.vazquezb@gmail.com>
2022-07-27 03:00:18 -0500

    Add action, function action creator and reducer to region.js file

---
+++ src/redux/region/region.js
@@ -0,0 +1,45 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+// Actions
+const FETCH_CARBON_SOURCE = 'capstone_3/region/FETCH_CARBON_SOURCE';
+
+// URL
+const regionURL = 'https://api.carbonintensity.org.uk/regional';
+
+// Async function (Function Action Creator)
+const fetchSrcAsync = createAsyncThunk(
+  FETCH_CARBON_SOURCE,
+  async () => {
+    const response = await fetch(regionURL);
+    const output = await response.json();
+    const regionsRaw = output.data[0].regions;
+
+    const regions = regionsRaw.map((region) => ({
+      id: region.regionid,
+      name: region.shortname,
+      intensity: region.intensity,
+      sources: region.generationmix,
+    }));
+
+    return regions;
+  },
+);
+
+const initialState = [];
+
+// Reducer
+const regionSlice = createSlice({
+  name: 'source',
+  initialState,
+  extraReducers: {
+    [fetchSrcAsync.fulfilled]: (state, action) => (
+      [...action.payload]
+    ),
+  },
+});
+
+export { fetchSrcAsync };
+
+// export const {} = intensitySlice.actions;
+
+export default regionSlice.reducer;
