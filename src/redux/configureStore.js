ca372079b2b84a001e25562cdf0457e9b83bda63
jevazquezb <je.vazquezb@gmail.com>
2022-07-27 13:22:32 -0500

    Add sources reducer

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import countryReducer from './country/country';
 import regionReducer from './region/region';
+import sourceReducer from './sources/sources';
 
 const store = configureStore({
   reducer: {
     country: countryReducer,
     region: regionReducer,
+    sources: sourceReducer,
   },
 });
 
