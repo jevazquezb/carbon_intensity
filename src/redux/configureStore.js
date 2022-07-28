31df1b4bd4f5e768c17f0481a18205aa723b7418
jevazquezb <je.vazquezb@gmail.com>
2022-07-28 08:02:29 -0500

    Add intensities reducer

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,12 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import countryReducer from './country/country';
 import regionReducer from './region/region';
+import intensityReducer from './intensities/intensities';
 import sourceReducer from './sources/sources';
 
 const store = configureStore({
   reducer: {
     country: countryReducer,
     region: regionReducer,
+    intensities: intensityReducer,
     sources: sourceReducer,
   },
 });
