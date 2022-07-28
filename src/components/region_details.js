e0537ca7c80a1198bf3c32b703fda1e49252329d
jevazquezb <je.vazquezb@gmail.com>
2022-07-28 07:54:35 -0500

    Add components to region_details.js

--- src/components/region_details.js
+++ src/components/region_details.js
@@ -5,17 +5,18 @@ import {
 } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import PropTypes from 'prop-types';
-// import { Icon } from '@iconify/react';
-import { fetchSrcAsync } from '../redux/sources/sources';
+import { Icon } from '@iconify/react';
+import { intAsync } from '../redux/intensities/intensities';
+import { fetchCarbonAsync } from '../redux/sources/sources';
 import {
   dir,
   displayCarbon,
   displayIntensity,
 } from './region_list';
 
-function DisplayRegion({ region }) {
-  let { name } = region;
-  const { forecast, intensity } = region;
+function DisplayRegion({ intensities }) {
+  let { name } = intensities;
+  const { forecast, intensity } = intensities;
 
   if (name === 'North Wales & Merseyside') name = 'North Wales';
   else if (name === 'GB') name = 'United Kingdom';
@@ -37,33 +38,88 @@ function DisplayRegion({ region }) {
   );
 }
 
+function SourceItem({ source }) {
+  const { fuel, perc } = source;
+  const upFuel = fuel[0].toUpperCase() + fuel.slice(1);
+  return (
+    <li className="fuel-cont">
+      <h4 className="fuel-title">{upFuel}</h4>
+      <p className="fuel-perc">{ `${perc} %` }</p>
+      <Icon
+        icon="uil:arrow-circle-right"
+        color="white"
+        className="region-link-icon"
+      />
+    </li>
+  );
+}
+
+function Sources({ sources }) {
+  const createList = () => {
+    const sourcesCopy = [...sources];
+    const orderedSources = sourcesCopy.sort((a, b) => b.perc - a.perc);
+    let count = 0;
+    const list = orderedSources.map((source) => {
+      count += 1;
+      return (
+        <SourceItem
+          source={source}
+          key={`${count}`}
+        />
+      );
+    });
+    return list;
+  };
+
+  return (
+    <section>
+      <h3 className="list-title">CARBON SOURCES</h3>
+      <ul>
+        { createList() }
+      </ul>
+    </section>
+  );
+}
+
 function RegionDetails() {
   const dispatch = useDispatch();
   const { id } = useParams();
-  const region = useSelector((state) => state.sources);
+  const intensities = useSelector((state) => state.intensities);
+  const sources = useSelector((state) => state.sources);
 
   useEffect(() => {
-    dispatch(fetchSrcAsync(id));
+    dispatch(intAsync(id));
+    dispatch(fetchCarbonAsync(id));
   }, []);
 
   return (
     <>
-      <DisplayRegion region={region} />
-      {/* <Sources /> */}
+      <DisplayRegion intensities={intensities} />
+      <Sources sources={sources} />
     </>
   );
 }
 
 DisplayRegion.propTypes = {
-  region: PropTypes.shape({
+  intensities: PropTypes.shape({
     id: PropTypes.number,
     name: PropTypes.string,
     forecast: PropTypes.number,
     intensity: PropTypes.string,
-    sources: PropTypes.arrayOf(PropTypes.shape({
-      fuel: PropTypes.string,
-      perc: PropTypes.number,
-    })),
+  }).isRequired,
+};
+
+Sources.propTypes = {
+  sources: PropTypes.arrayOf(PropTypes.shape({
+    fuel: PropTypes.string,
+    perc: PropTypes.number,
+  })).isRequired,
+};
+
+SourceItem.propTypes = {
+  source: PropTypes.shape({
+    fuel: PropTypes.string,
+    perc: PropTypes.number,
   }).isRequired,
 };
 
