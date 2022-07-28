c94ad48ee604977a804141793d8a52d8487276af
jevazquezb <je.vazquezb@gmail.com>
2022-07-28 08:01:54 -0500

    Update action, action creator and reducer from sources.js

--- src/redux/sources/sources.js
+++ src/redux/sources/sources.js
@@ -3,41 +3,32 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 // Actions
 const FETCH_CARBON_SOURCE = 'capstone_3/sources/FETCH_CARBON_SOURCE';
 
-// URL
-
 // Async function (Function Action Creator)
-const fetchSrcAsync = createAsyncThunk(
+const fetchCarbonAsync = createAsyncThunk(
   FETCH_CARBON_SOURCE,
   async (id) => {
     const singleRegionURL = `https://api.carbonintensity.org.uk/regional/regionid/${id}`;
     const response = await fetch(singleRegionURL);
     const output = await response.json();
     const regionRaw = output.data[0];
-    const region = {
-      id: regionRaw.regionid,
-      name: regionRaw.shortname,
-      forecast: regionRaw.data[0].intensity.forecast,
-      intensity: regionRaw.data[0].intensity.index,
-      sources: regionRaw.data[0].generationmix,
-    };
-
-    return region;
+    const regionSrc = regionRaw.data[0].generationmix;
+    return regionSrc;
   },
 );
 
-const initialState = {};
+const initialState = [];
 
 // Reducer
 const sourceSlice = createSlice({
   name: 'sources',
   initialState,
   extraReducers: {
-    [fetchSrcAsync.fulfilled]: (state, action) => (
-      { ...state, ...action.payload }
+    [fetchCarbonAsync.fulfilled]: (state, action) => (
+      [...action.payload]
     ),
   },
 });
 
-export { fetchSrcAsync };
+export { fetchCarbonAsync };
 
 export default sourceSlice.reducer;
